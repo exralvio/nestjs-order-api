dd09351cd49e6aca7a1d630ee86a84b15d01c255
Alvio Leegia <exralvio@gmail.com>
2025-10-30 20:46:51 +0700

    Remove dummy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
 import { TenantMigrationService } from './prisma/tenant-migration.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { RolesGuard } from './auth/guards/roles.guard';
@@ -10,14 +9,8 @@ import { ApiOperation } from '@nestjs/swagger';
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
     private readonly tenantMigrationService: TenantMigrationService,
   ) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   @Get('migrate-tenants')
   async migrateAllTenants(): Promise<any> {
